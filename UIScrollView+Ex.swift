ed697be430b56e23ee5e27624b3d7bfe6596057a
iAugus <iAugux@gmail.com>
2016-08-25 16:25:39 +0800

    -

--- UIScrollView+Ex.swift
+++ UIScrollView+Ex.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
-
+import SnapKit
 
 extension UIScrollView {
     
-    func scrollToTop(animated: Bool = true) {
-        let point = CGPoint(x: 0, y: -contentInset.top)
-        setContentOffset(point, animated: animated)
+    @discardableResult
+    func scrollToTop(animated: Bool = true) -> CGPoint {
+        let topOffset = CGPoint(x: 0, y: -contentInset.top)
+        setContentOffset(topOffset, animated: animated)
+        return topOffset
+    }
+    
+    @discardableResult
+    func scrollToBottom(animated: Bool = true) -> CGPoint {
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height)
+        setContentOffset(bottomOffset, animated: animated)
+        return bottomOffset
+    }
+    
+    @discardableResult
+    func scrollToTopOrBottomAutomatically(animated: Bool = true) -> CGPoint {
+        
+        let topOffset = CGPoint(x: 0, y: -contentInset.top)
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height)
+
+        let offset = contentOffset == bottomOffset ? topOffset : bottomOffset
+        setContentOffset(offset, animated: animated)
+        return offset
+    }
+    
+    @objc fileprivate func didTriggerOff() {
+        scrollToTopOrBottomAutomatically()
+    }
+    
+    func configureRightBottomCornerScrollTrigger(superView: UIView? = nil) {
+        
+        let view: UIView
+        
+        if superView != nil {
+            view = superView!
+        } else {
+            guard let sv = self.superview else { return }
+            view = sv
+        }
+        
+        print(view)
+        
+        let v = UIView()
+        view.addSubview(v)
+        v.snp.makeConstraints { (make) in
+            make.width.height.equalTo(44)
+            make.right.bottom.equalTo(8)
+        }
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(didTriggerOff))
+        v.addGestureRecognizer(recognizer)
+        v.isUserInteractionEnabled = true
     }
 }
