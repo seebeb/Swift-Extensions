6024c31ea9e04e81246e6cc1a225e4cc17ea2404
iAugus <iAugux@gmail.com>
2016-08-31 23:44:28 +0800

    -

--- UIStoryboard+Ex.swift
+++ UIStoryboard+Ex.swift
@@ -51,6 +51,32 @@ extension UITableView {
 
 // MARK: - 
 
+extension UICollectionView {
+    
+    /// Returns a reusable table-view cell object for the specified reuse identifier and adds it to the table.
+    /// Note: withIdentifier must be equal to the Cell Class.
+    func dequeueReusableCell<T : UICollectionViewCell>(with cell: T.Type, for indexPath: IndexPath) -> T {
+        return dequeueReusableCell(withReuseIdentifier: String(describing: cell.self), for: indexPath) as! T
+    }
+    
+    /// Registers a nib object containing a cell with the table view under a specified identifier.
+    /// Nib name must be equal to the Cell Class, and the forCellReuseIdentifier must equal to Cell Class as well.
+    func registerNib(_ cell: Swift.AnyClass) {
+        let id = String(describing: cell.self)
+        let nib = UINib(nibName: id, bundle: nil)
+        register(nib, forCellWithReuseIdentifier: id)
+    }
+    
+    /// Registers a class for use in creating new table cells.
+    /// Note: forCellReuseIdentifier must equal to the Cell Class.
+    func register(_ cellClass: Swift.AnyClass) {
+        register(cellClass, forCellWithReuseIdentifier: String(describing: cellClass))
+    }
+}
+
+
+// MARK: - 
+
 extension UIView {
     
     /// Load view from nib. Note: Nib name must be equal to the class name.
