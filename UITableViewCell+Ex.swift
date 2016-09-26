7ed0e3cd020b6e4e7ab5b1dc8427a13586deece5
iAugus <iAugux@gmail.com>
2016-09-26 19:59:45 +0800

    -

---
+++ UITableViewCell+Ex.swift
@@ -0,0 +1,24 @@
+//
+//  UITableViewCell+Ex.swift
+//  iTumblr
+//
+//  Created by Augus on 9/26/16.
+//  Copyright © 2016 iAugus. All rights reserved.
+//
+
+import UIKit
+
+
+extension UITableViewCell {
+
+    var relatedTableView: UITableView? {
+
+        var view = superview
+
+        while view != nil && !(view is UITableView) {
+            view = view?.superview
+        }
+
+        return view as? UITableView
+    }
+}
