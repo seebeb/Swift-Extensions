aa704aa4ffbd4b23c837817cf641d0deb392387e
iAugus <iAugux@gmail.com>
2016-10-10 01:46:50 +0800

    -

--- UIScrollView+Ex.swift
+++ UIScrollView+Ex.swift
@@ -64,36 +64,83 @@ extension UIScrollView {
     @objc fileprivate func didTriggerOff() {
         scrollToTopOrBottomAutomatically()
     }
-    
-    func configureRightBottomCornerScrollTrigger(superView: UIView? = nil, color: UIColor? = nil, radius: CGFloat = 4) {
+
+    @discardableResult
+    func configureRightBottomCornerScrollTrigger(superView: UIView? = nil, color: UIColor? = nil, radius: CGFloat = 4) -> UIView? {
         
         let view: UIView
         
         if superView != nil {
             view = superView!
         } else {
-            guard let sv = self.superview else { return }
+            guard let sv = self.superview else { return nil }
             view = sv
         }
         
         let v = UIView()
         view.addSubview(v)
-        v.snp.makeConstraints { (make) in
-            make.width.height.equalTo(44)
-            make.right.bottom.equalTo(0)
+        v.snp.makeConstraints {
+            $0.width.height.equalTo(44)
+            $0.right.bottom.equalTo(0)
         }
-        
-        if let color = color {
-            let center = CGPoint(x: 44, y: 44)
-            let circlePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat(M_PI * 2), clockwise: true)
-            let shapeLayer = CAShapeLayer()
-            shapeLayer.path = circlePath.cgPath
-            shapeLayer.fillColor = color.cgColor
-            v.layer.addSublayer(shapeLayer)
+
+        var color = color
+
+        if color == nil {
+            color = color ?? view.tintColor ?? AppDelegate().window?.tintColor ?? .black
+            color = color?.withAlphaComponent(0.8)
         }
-        
+
+        let center = CGPoint(x: 44, y: 44)
+        let circlePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat(M_PI * 2), clockwise: true)
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        shapeLayer.fillColor = color!.cgColor
+        v.layer.addSublayer(shapeLayer)
+
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(didTriggerOff))
         v.addGestureRecognizer(recognizer)
         v.isUserInteractionEnabled = true
+
+        return v
+    }
+
+    @discardableResult
+    func configureLeftBottomCornerTrigger(target: Any?, action: Selector, superView: UIView? = nil, color: UIColor? = nil, radius: CGFloat = 4) -> UIButton? {
+
+        let view: UIView
+
+        if superView != nil {
+            view = superView!
+        } else {
+            guard let sv = self.superview else { return nil }
+            view = sv
+        }
+
+        let b = UIButton()
+        b.backgroundColor = .clear
+        b.isUserInteractionEnabled = true
+        b.addTarget(target, action: action, for: .touchUpInside)
+        view.addSubview(b)
+        b.snp.makeConstraints() {
+            $0.width.height.equalTo(44)
+            $0.left.bottom.equalTo(0)
+        }
+
+        var color = color
+
+        if color == nil {
+            color = color ?? view.tintColor ?? AppDelegate().window?.tintColor ?? .black
+            color = color?.withAlphaComponent(0.8)
+        }
+
+        let center = CGPoint(x: 0, y: 44)
+        let circlePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat(M_PI * 2), clockwise: true)
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        shapeLayer.fillColor = color!.cgColor
+        b.layer.addSublayer(shapeLayer)
+
+        return b
     }
 }
