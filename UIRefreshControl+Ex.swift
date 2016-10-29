7c4b5fe85354c581c05e82ef843bdeaf986db6bd
iAugus <iAugux@gmail.com>
2016-10-29 22:08:35 +0800

    -

---
+++ UIRefreshControl+Ex.swift
@@ -0,0 +1,23 @@
+//
+//  UIRefreshControl+Ex.swift
+//
+//  Created by Augus on 10/29/16.
+//  Copyright © 2016 iAugus. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIRefreshControl {
+
+    func beginRefreshingManually() {
+
+        guard let scrollView = superview as? UIScrollView else { return }
+
+        let offsetY = scrollView.contentOffset.y - frame.height
+
+        scrollView.setContentOffset(CGPoint(x: 0, y: offsetY), animated: true)
+
+        beginRefreshing()
+    }
+}
