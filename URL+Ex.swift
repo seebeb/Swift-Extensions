7773115e3853c9f7184cd9134aec4272a7f2bfd7
iAugus <iAugux@gmail.com>
2016-12-18 03:06:39 +0800

    -

---
+++ URL+Ex.swift
@@ -0,0 +1,41 @@
+//
+//  URL+Ex.swift
+//  iTumblr
+//
+//  Created by Augus on 12/17/16.
+//  Copyright © 2016 iAugus. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+
+    func httpsURL() -> URL {
+
+        guard scheme != "https" else {
+            return self
+        }
+
+        let str = absoluteString.replacingOccurrences(of: "http://", with: "https://")
+
+        return URL(string: str)!
+    }
+}
+
+extension String {
+
+    func toURL(forceToHttps: Bool = false) -> URL? {
+        if forceToHttps {
+            return URL(string: self)?.httpsURL()
+        } else {
+            return URL(string: self)
+        }
+    }
+
+    func httpsURL() -> URL? {
+
+        guard let url = URL(string: self) else { return nil }
+        
+        return url.httpsURL()
+    }
+}
