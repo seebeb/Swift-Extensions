13157ade481202ed729e61259789ba2d0ec31406
iAugus <iAugux@gmail.com>
2017-01-08 17:33:49 +0800

    -

---
+++ UITableView+Ex.swift
@@ -0,0 +1,23 @@
+//
+//  UITableView+Ex.swift
+//  iTumblr
+//
+//  Created by Augus on 1/7/17.
+//  Copyright © 2017 iAugus. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+
+    func reloadData(with animation: UITableViewRowAnimation) {
+        reloadSections(IndexSet(integersIn: 0 ..< numberOfSections), with: animation)
+    }
+
+    func reloadVisibleRows(with animation: UITableViewRowAnimation = .none) {
+        
+        guard let ips = indexPathsForVisibleRows else { return }
+
+        reloadRows(at: ips, with: animation)
+    }
+}
