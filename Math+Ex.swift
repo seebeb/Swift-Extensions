7b2f1ce400432b47763fe7fc56f751fdcf6d1431
iAugus <iAugux@gmail.com>
2017-03-15 02:14:38 +0800

    -

--- Math+Ex.swift
+++ Math+Ex.swift
@@ -111,6 +111,21 @@ extension Int {
 }
 
 
+// MARK: - 
+
+extension Float {
+    var cleanValue: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
+
+extension Double {
+    var cleanValue: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
+
+
 // MARK: - Calculations
 
 // MARK: - Total
