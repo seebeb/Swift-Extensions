4ac74a9371ba73c378e0492451d1666e1b682cb6
iAugus <iAugux@gmail.com>
2018-04-29 20:22:32 +0800

    -

--- UIColor+Hex.swift
+++ UIColor+Hex.swift
@@ -112,6 +112,20 @@ extension UIColor {
             throw UIColorInputError.mismatchedHexStringLength
         }
     }
+
+    public convenience init?(hex: String, alpha: CGFloat = 1.0) {
+        var formatted = hex.replacingOccurrences(of: "0x", with: "")
+        formatted = formatted.replacingOccurrences(of: "#", with: "")
+        if let hex = Int(formatted, radix: 16) {
+            let red = CGFloat(CGFloat((hex & 0xFF0000) >> 16) / 255.0)
+            let green = CGFloat(CGFloat((hex & 0x00FF00) >> 8) / 255.0)
+            let blue = CGFloat(CGFloat((hex & 0x0000FF) >> 0) / 255.0)
+            self.init(red: red, green: green, blue: blue, alpha: alpha)
+
+        } else {
+            return nil
+        }
+    }
     
     /**
      The rgba string representation of color with alpha of the form #RRGGBBAA/#RRGGBB, fails to default color.
