bbe9224afca877169b6b8a5d8b2f28be08c642d3
iAugus <iAugux@gmail.com>
2018-05-31 15:31:34 +0800

    -

---
+++ UIButton+EventClosure.swift
@@ -0,0 +1,48 @@
+//
+//  UIButton+EventClosure.swift
+//
+//  Created by WangLei on 2018/5/31.
+//  Copyright © 2018 Happy Iterating Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func addTarget(for event: UIControlEvents = .touchUpInside, closure: @escaping UIButtonTargetClosure) {
+        targetClosure = closure
+        addTarget(self, action: #selector(closureAction), for: event)
+    }
+}
+
+typealias UIButtonTargetClosure = (UIButton) -> ()
+
+private class ClosureWrapper: NSObject {
+    let closure: UIButtonTargetClosure
+    init(_ closure: @escaping UIButtonTargetClosure) {
+        self.closure = closure
+    }
+}
+
+private extension UIButton {
+    
+    struct AssociatedKeys {
+        static var targetClosure = "targetClosure"
+    }
+    
+    var targetClosure: UIButtonTargetClosure? {
+        get {
+            return (objc_getAssociatedObject(self, &AssociatedKeys.targetClosure) as? ClosureWrapper)?.closure
+        }
+        set {
+            guard let newValue = newValue else { return }
+            objc_setAssociatedObject(self,
+                                     &AssociatedKeys.targetClosure, ClosureWrapper(newValue),
+                                     objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    @objc func closureAction() {
+        targetClosure?(self)
+    }
+}
