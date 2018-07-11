4d12e84ba256c2cef6c4225aa3ee32f5765a8bf8
iAugus <iAugux@gmail.com>
2018-07-11 14:06:13 +0800

    -

--- UINavigationBar+Ex.swift
+++ UINavigationBar+Ex.swift
@@ -20,39 +20,78 @@ extension UINavigationBar {
 }
 
 extension UINavigationBar {
-
+    
     private struct Association {
         static var key = 0
     }
     
+    /// Support iOS 12
     var isBottomHairlineHidden: Bool {
         get {
             return objc_getAssociatedObject(self, &Association.key) as? Bool ?? false
         }
         set {
             objc_setAssociatedObject(self, &Association.key, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
-            navigationBarImageView?.isHidden = newValue
-        }
-    }
-    
-    fileprivate var navigationBarImageView: UIImageView? {
-        return hairlineImageViewInNavigationBar(self)
-    }
-    
-    fileprivate func hairlineImageViewInNavigationBar(_ view: UIView) -> UIImageView? {
-        
-        if view is UIImageView && view.bounds.height <= 1.0 {
-            return view as? UIImageView
-        }
-        
-        let subviews = view.subviews as [UIView]
-        for subview: UIView in subviews {
-            if let imageView = hairlineImageViewInNavigationBar(subview) {
-                return imageView
+            
+            var imageView: UIImageView?
+            
+            func loop() {
+                loopDescendantViews() {
+                    if let iv = $0 as? UIImageView, iv.bounds.height <= 1.0 {
+                        imageView = iv
+                        return
+                    }
+                }
+            }
+            
+            guard imageView == nil else {
+                imageView?.isHidden = newValue
+                return
+            }
+            
+            executeAfterDelay(0.1) {
+                loop()
+                imageView?.isHidden = newValue
             }
         }
-        
-        return nil
     }
-    
 }
+
+
+//extension UINavigationBar {
+//
+//    private struct Association {
+//        static var key = 0
+//    }
+//    
+//    var isBottomHairlineHidden: Bool {
+//        get {
+//            return objc_getAssociatedObject(self, &Association.key) as? Bool ?? false
+//        }
+//        set {
+//            objc_setAssociatedObject(self, &Association.key, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+//            navigationBarImageView?.isHidden = newValue
+//        }
+//    }
+//    
+//    fileprivate var navigationBarImageView: UIImageView? {
+//        return hairlineImageViewInNavigationBar(self)
+//    }
+//    
+//    fileprivate func hairlineImageViewInNavigationBar(_ view: UIView) -> UIImageView? {
+//        
+//        if view is UIImageView && view.bounds.height <= 1.0 {
+//            return view as? UIImageView
+//        }
+//        
+//        let subviews = view.subviews as [UIView]
+//        for subview: UIView in subviews {
+//            if let imageView = hairlineImageViewInNavigationBar(subview) {
+//                return imageView
+//            }
+//        }
+//        
+//        return nil
+//    }
+//    
+//}
